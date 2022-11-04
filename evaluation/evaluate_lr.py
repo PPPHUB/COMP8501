e7a5ecc5ade35c6390ae4a737606d8463b32e068
Wang, Shihao (Australian National University) <u7162427@anu.edu.au>
2022-11-05 08:20:31 +1100

    s

--- evaluation/evaluate_lr.py
+++ evaluation/evaluate_lr.py
@@ -82,7 +82,7 @@ class Evaluator:
         self.results = [(dataset, clip, future.result()) for dataset, clip, future in tasks]
         
     def write_excel(self):
-        workbook = xlsxwriter.Workbook(os.path.join(self.args.pred_dir, f'{os.path.basename(self.args.pred_dir)}.xlsx'))
+        workbook = xlsxwriter.Workbook(os.path.join("/content", f'{os.path.basename(self.args.pred_dir)}.xlsx'))
         summarysheet = workbook.add_worksheet('summary')
         metricsheets = [workbook.add_worksheet(metric) for metric in self.results[0][2].keys()]
         
