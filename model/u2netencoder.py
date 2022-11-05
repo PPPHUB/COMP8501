672a255385ea15347364efe3ddb17b02db4df9ec
Wang, Shihao (Australian National University) <u7162427@anu.edu.au>
2022-11-06 02:14:02 +1100

    s

--- model/u2netencoder.py
+++ model/u2netencoder.py
@@ -366,7 +366,7 @@ class U2NET(nn.Module):
     self.outconv = nn.Conv2d(6*out_ch, out_ch, 1)
     if pretrained:
       print("loaded")
-      self.load_state_dict(torch.load("/content/drive/Mydrive/checkpoint.pth"))
+      self.load_state_dict(torch.load("/content/drive/MyDrive/checkpoint.pth"))
   def forward(self, x):
 
     hx = x
